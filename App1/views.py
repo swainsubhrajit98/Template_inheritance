8ca9a760072b18bd6d450c9d336829562164dbf6
Swagat Swain <ssswagatss@gmail.com>
2022-10-13 14:30:52 +0530

    first commit

---
+++ App1/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+def Parent(request):
+    return render(request,'Parent.html')
+
+def Child(request):
+    return render(request,'Child.html')
